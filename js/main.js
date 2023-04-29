7fc34a4debda576fe4d7efa75a5bbc430b549b61
Héctor Mateu <hmateu.ortola@gmail.com>
2023-04-29 19:19:43 +0200

    fix: Implementa el Javascript directamente en el HTML

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function screenOn(){
-    let screen = document.getElementById("nintendoWallpaper");
+// function screenOn(){
+//     let screen = document.getElementById("nintendoWallpaper");
 
-    if(screen.src.match("./img/transparent.png")){
-        screen.src = "../img/powerOn.gif";
-    }else{
-        screen.src = "../img/transparent.png";
-    }
-}
+//     if(screen.src.match("./img/transparent.png")){
+//         screen.src = "../img/powerOn.gif";
+//     }else{
+//         screen.src = "../img/transparent.png";
+//     }
+// }
