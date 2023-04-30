8fe5f41ff83af3a1161526de7f908947b67d88d4
Héctor Mateu <125643668+hmateu@users.noreply.github.com>
2023-04-30 09:13:51 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// function screenOn(){
-//     let screen = document.getElementById("nintendoWallpaper");
-
-//     if(screen.src.match("./img/transparent.png")){
-//         screen.src = "../img/powerOn.gif";
-//     }else{
-//         screen.src = "../img/transparent.png";
-//     }
-// }
